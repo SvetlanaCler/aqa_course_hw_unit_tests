e0d788506f9adacff7edd5136970410b93aea984
SvetlanaCler <clerforlani@hotmail.com>
2025-04-06 18:21:06 +0300

    'task3'

--- homework-solutions/js/hw-6/task-3.mjs
+++ homework-solutions/js/hw-6/task-3.mjs
@@ -6,6 +6,17 @@
 
   Присвойте результат в переменную "unique"
 */
+
+
 let unique;
  
+unique = [];
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 4, 6, 8, 10, 1, 3, 5, 7, 9];
+for(let i=0;i<arr.length;i++) {
+  if(!unique.includes(arr[i]){
+    unique.push(arr[i]);
+  })
+}
+
+console.log(unique);
 export { unique };
